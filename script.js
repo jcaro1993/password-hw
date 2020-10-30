3ee8d87d67afe123b293b724128ed010b1a62214
Javier Caro <jcaro1993@gmail.com>
2020-10-30 16:16:43 -0700

    built PW generate and added funtionality

--- script.js
+++ script.js
@@ -1,14 +1,64 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
+var num = "0123456789";
+
+var result = []
+upperCase = upperCase.split("")
+lowerCase = lowerCase.split("")
+specialChar = specialChar.split("")
+num = num.split("")
+
+alert("Welcome! Please click 'Generate password' to start!");
+
+function generatePassword() {
+
+    var pwlength = prompt("How long do you want your password to be");
+    var newPass = ""
+    var letterChoice = confirm("do you want ypur password to have letters?")
+    var numChoice = confirm("do you want your password to have numbers?")
+    var symbolChoice = confirm("do you want your password to have special letters")
+    if (pwlength < 8 && pwlength > 128) {
+        alert("Password should be between 8 and 128 Characters")
+
+    } else {
+        if (letterChoice == true) {
+            for (var i = 0; i < upperCase.length; i++) {
+                result.push(upperCase[i])
+            }
+            for (var i = 0; i < lowerCase.length; i++) {
+                result.push(lowerCase[i])
+            }
+        }
+        if (numChoice == true) {
+            for (var i = 0; i < num.length; i++) {
+                result.push(num[i])
+            }
+        }
+        if (symbolChoice == true) {
+            for (var i = 0; i < specialChar.length; i++) {
+                result.push(specialChar[i])
+            }
+        }
+
+        // create a loop that loops pwlength * and gets a random char from my results array and uses that to creat final PW
+        for (var i = 0; i < pwlength; i++) {
+            newPass += result[Math.floor(Math.random() * result.length)]
+        }
+    }
+    return newPass
+}
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
